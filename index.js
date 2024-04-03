678f4ebdd871ff306dd8329d73f7eaddd3b91932
lynn <lynnnduta23@gmail.com>
2024-04-03 14:36:08 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.querySelector('main#main')
+document.body.removeChild(main)
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'Lynn is the champion'
+document.body.appendChild(newHeader)
+
+
